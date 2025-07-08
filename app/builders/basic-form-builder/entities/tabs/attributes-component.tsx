ac835e33ec9f056ae558f8248203004cab715cdd
Nurym <54814137+Nurymadil@users.noreply.github.com>
2025-07-08 21:47:14 +0500

    Switch tabs to PrimeReact

--- app/builders/basic-form-builder/entities/tabs/attributes-component.tsx
+++ app/builders/basic-form-builder/entities/tabs/attributes-component.tsx
@@ -1,9 +1,11 @@
 import { TabLabelsAttribute } from "../../attributes/tab-labels/component";
+import { LazyAttribute } from "../../attributes/lazy/component";
 
 export function TabsAttributes() {
   return (
     <>
       <TabLabelsAttribute />
+      <LazyAttribute />
     </>
   );
 }
